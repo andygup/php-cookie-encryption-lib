e4bec24c0dfb63efb1fc5fb06ec9999a9e4fefcb
andygup <agup@esri.com>
2013-04-16 16:29:22 -0600

    minor formatting changes

--- Encrypt.php
+++ Encrypt.php
@@ -10,31 +10,31 @@ class Encrypt{
 	
     function __construct($encryption_key,$iv,$time_zone) {
         $this->key = $encryption_key;
-		$this->iv = $iv;
-		date_default_timezone_set($time_zone);
+        $this->iv = $iv;
+        date_default_timezone_set($time_zone);
     }
 
     public function encrypt($value){
         $final = null;
-		if($value == null || $this->key == null || $this->iv == null)
-		{
-			header("HTTP/1.0 400 Error");
-			echo "\n\n\nERROR: Null value detected: check your inputs";
-			exit;
-		}
-		else
-		{
-			try
-			{
-				$final = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->key, $value, MCRYPT_MODE_CFB,$this->iv);
-			}
-			catch(Exception $e)
-			{
-				header("HTTP/1.0 400 Error");
-				echo "\n\n\nERROR: Failed encryption. " .$e->getMessage();
-				exit;
-			}		
-		}
+        if($value == null || $this->key == null || $this->iv == null)
+        {
+            header("HTTP/1.0 400 Error");
+            echo "\n\n\nERROR: Null value detected: check your inputs";
+            exit;
+        }
+        else
+        {
+            try
+            {
+                $final = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->key, $value, MCRYPT_MODE_CFB,$this->iv);
+            }
+            catch(Exception $e)
+            {
+                header("HTTP/1.0 400 Error");
+                echo "\n\n\nERROR: Failed encryption. " .$e->getMessage();
+                exit;
+             }		
+        }
 
         return  $final;
     }
@@ -48,9 +48,9 @@ class Encrypt{
         return base64_encode($result);
     }
 	
-	public function get_IV(){
-		$size = mcrypt_get_iv_size(MCRYPT_CAST_256, MCRYPT_MODE_CFB);
-		return base64_encode(mcrypt_create_iv($size, MCRYPT_RAND)); //You can try other sources
-	}
+    public function get_IV(){
+        $size = mcrypt_get_iv_size(MCRYPT_CAST_256, MCRYPT_MODE_CFB);
+	    return base64_encode(mcrypt_create_iv($size, MCRYPT_RAND)); //You can try other sources
+    }
 }
 ?>
