67d2f059b087cedbf9d8ea5e74b286e8daab15a2
andygup <agup@esri.com>
2013-04-16 16:25:00 -0600

    v1

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('OAUTH_COOKIE_DOMAIN', '');
+
+//REQUIRED - Encrypt your cookies
+//http://si0.twimg.com/images/dev/oauth_diagram.png
+//Create your own unique ENCRYPTION_KEY via Encrypt.get_RandomKey()
+define('ENCRYPTION_KEY','Q83dBef2tgmHKZ9T1htFA2Y+jZgdler0szN28rjBf8o='); 
+//Create your own unique initialization vector via Encrypt.get_IV()
+define('IV','C2Oez0DLMQ8rCcgYFJwzCw==');
+define('DEFAULT_TIME_ZONE','America/Los_Angeles');
